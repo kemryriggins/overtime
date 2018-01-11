4e3087e40a86c259fdf49bd6e7d79a73652d017e
kemryriggins <98kariggins@gmail.com>
2018-01-11 13:49:52 -0700

    changed the approval workflow editing function

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 describe 'navigate' do
-	before do
-		@admin_user = FactoryGirl.create(:admin_user)
-		login_as(@admin_user, :scope => :user)
-	end
+  before do
+    @admin_user = FactoryGirl.create(:admin_user)
+    login_as(@admin_user, :scope => :user)
+  end
 
-	describe 'edit' do
+  describe 'edit' do
     before do
       @post = FactoryGirl.create(:post)
       visit edit_post_path(@post)
@@ -28,5 +28,17 @@ describe 'navigate' do
 
       expect(page).to_not have_content('Approved')
     end
-	end
+
+    it 'should not be editable by the post creator if status is approved' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+
+      @post.update(user_id: user.id, status: 'approved')
+
+      visit edit_post_path(@post)
+
+      expect(current_path).to eq(root_path)
+    end
+  end
 end
