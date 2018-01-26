3cb6eb0f25f0fae9c815d17a7a9257aaea8c5135
kemryriggins <98kariggins@gmail.com>
2018-01-26 11:26:23 -0700

    Updated notifications on heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 
 puts "1 User created"
 
-AdminUser.create(email: "admin@test.com",
+AdminUser.create(email: "98kariggins@gmail.com",
 									password: "asdfasdf",
 									password_confirmation: "asdfasdf",
 									first_name: "Admin",
